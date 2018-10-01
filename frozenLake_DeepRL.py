835634f56f3043630988db01a9e3fb23546201d8
Luiz Ricardo Takeshi Horita <horita.l@sidiamao.com.br>
2018-10-01 10:17:24 -0300

    Add bias to network.

--- frozenLake_DeepRL.py
+++ frozenLake_DeepRL.py
@@ -15,7 +15,8 @@ tf.reset_default_graph()
 #These lines establish the feed-forward part of the network used to choose actions
 inputs1 = tf.placeholder(shape=[1,16],dtype=tf.float32)
 W = tf.Variable(tf.random_uniform([16,4],0,0.01))
-Qout = tf.matmul(inputs1,W)
+b = tf.Variable(tf.random_uniform([4,1], 0, 0.01))
+Qout = tf.matmul(inputs1,W) + b
 predict = tf.argmax(Qout,1)
 
 #Below we obtain the loss by taking the sum of squares difference between the target and prediction Q values.
